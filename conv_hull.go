96a5688a3d433c3b98dd687e8e3db4c3f1359d75
Denis Strizhkin <strdenis02@gmail.com>
2024-08-16 22:41:08 +0300

    fix what I've just broken :)

--- conv_hull.go
+++ conv_hull.go
@@ -13,7 +13,7 @@ func (p Point) Subtract(q Point) Point {
 }
 
 func (p Point) VectMult(q Point) float64 {
-	return (p.X*q.Y - p.Y*q.X)
+	return p.X*q.Y - p.Y*q.X
 }
 
 func NewConvexHull(points []Point) *ConvexHull {
@@ -21,20 +21,28 @@ func NewConvexHull(points []Point) *ConvexHull {
 	L_up := points[:2]
 	for i := 2; i < len(points); i++ {
 		L_up = append(L_up, points[i])
-		for len(L_up) > 2 {
-			if points[i].Subtract(points[i-1]).VectMult(points[i-1].Subtract(points[i-2])) > 0 {
+		for {
+			v12 := points[i].Subtract(points[i-1])
+			v23 := points[i-1].Subtract(points[i-2])
+			if v12.VectMult(v23) > 0 {
 				L_up = append(L_up[:i-2], L_up[i])
-				break
+				if len(L_up) > 2 {
+					break
+				}
 			}
 		}
 	}
 	L_low := []Point{points[len(points)-1], points[len(points)-2]}
 	for i := len(points) - 3; i >= 0; i-- {
 		L_low = append(L_low, points[i])
-		for len(L_low) > 2 {
-			if points[i].Subtract(points[i+1]).VectMult(points[i+1].Subtract(points[i+2])) > 0 {
+		for {
+			v12 := points[i].Subtract(points[i+1])
+			v23 := points[i+1].Subtract(points[i+2])
+			if v12.VectMult(v23) > 0 {
 				L_low = append(L_low[:i-2], L_low[i])
-				break
+				if len(L_low) > 2 {
+					break
+				}
 			}
 		}
 	}
