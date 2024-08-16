cf66db04f61ef35fc448b57c580ff108b84d3fde
Denis Strizhkin <strdenis02@gmail.com>
2024-08-16 19:16:21 +0000

    Update conv_hull.go
    Simpler code for NewConvexHull

--- conv_hull.go
+++ conv_hull.go
@@ -12,35 +12,29 @@ func (p Point) Subtract(q Point) Point {
 	return Point{p.X - q.X, p.Y - q.Y}
 }
 
-func (vec1 Point) VectMult(vec2 Point) float64 {
-	return (vec1.X*vec2.Y - vec1.Y*vec2.X)
+func (p Point) VectMult(q Point) float64 {
+	return (p.X*q.Y - p.Y*q.X)
 }
 
 func NewConvexHull(points []Point) *ConvexHull {
 	sort.Sort(ByPointX(points))
-	L_up := make([]Point, 0)
-	L_up = append(L_up, points[0], points[1])
+	L_up := points[:2]
 	for i := 2; i < len(points); i++ {
 		L_up = append(L_up, points[i])
-		right_turn := false
-		for len(L_up) > 2 && right_turn == false {
-			right_turn = true
-			if points[i].Subtract(points[i-1]).VectMult(points[i-1].Subtract(points[i-2])) <= 0 {
-				right_turn = false
+		for len(L_up) > 2 {
+			if points[i].Subtract(points[i-1]).VectMult(points[i-1].Subtract(points[i-2])) > 0 {
 				L_up = append(L_up[:i-2], L_up[i])
+				break
 			}
 		}
 	}
-	L_low := make([]Point, 0)
-	L_low = append(L_low, points[len(points)-1], points[len(points)-2])
+	L_low := []Point{points[len(points)-1], points[len(points)-2]}
 	for i := len(points) - 3; i >= 0; i-- {
 		L_low = append(L_low, points[i])
-		right_turn := false
-		for len(L_low) > 2 && right_turn == false {
-			right_turn = true
-			if points[i].Subtract(points[i+1]).VectMult(points[i+1].Subtract(points[i+2])) <= 0 {
-				right_turn = false
+		for len(L_low) > 2 {
+			if points[i].Subtract(points[i+1]).VectMult(points[i+1].Subtract(points[i+2])) > 0 {
 				L_low = append(L_low[:i-2], L_low[i])
+				break
 			}
 		}
 	}
