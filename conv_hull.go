14d82d3f377cb2f9445f286b1656a8c44ac7e17a
Denis Strizhkin <strdenis02@gmail.com>
2024-08-16 23:09:02 +0300

    simplify NewConfexHull even more

--- conv_hull.go
+++ conv_hull.go
@@ -18,34 +18,22 @@ func (p Point) VectMult(q Point) float64 {
 
 func NewConvexHull(points []Point) *ConvexHull {
 	sort.Sort(ByPointX(points))
+	is_left_turn := func(a, b, c Point) bool {
+		return a.Subtract(b).VectMult(b.Subtract(c)) > 0
+	}
 	L_up := points[:2]
 	for i := 2; i < len(points); i++ {
 		L_up = append(L_up, points[i])
-		for {
-			v12 := points[i].Subtract(points[i-1])
-			v23 := points[i-1].Subtract(points[i-2])
-			if v12.VectMult(v23) > 0 {
-				L_up = append(L_up[:i-2], L_up[i])
-				if len(L_up) > 2 {
-					break
-				}
-			}
+		for len(L_up) > 2 && is_left_turn(points[i], points[i-1], points[i-2]) {
+			L_up = append(L_up[:i-2], L_up[i])
 		}
 	}
 	L_low := []Point{points[len(points)-1], points[len(points)-2]}
 	for i := len(points) - 3; i >= 0; i-- {
 		L_low = append(L_low, points[i])
-		for {
-			v12 := points[i].Subtract(points[i+1])
-			v23 := points[i+1].Subtract(points[i+2])
-			if v12.VectMult(v23) > 0 {
-				L_low = append(L_low[:i-2], L_low[i])
-				if len(L_low) > 2 {
-					break
-				}
-			}
+		for len(L_low) > 2 && is_left_turn(points[i], points[i+1], points[i+3]) {
+			L_low = append(L_low[:i-2], L_low[i])
 		}
 	}
-	L := append(L_up, L_low[1:len(L_low)-1]...)
-	return &ConvexHull{L}
+	return &ConvexHull{append(L_up, L_low[1:len(L_low)-1]...)}
 }
