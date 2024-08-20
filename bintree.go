788daa6ac3dd70eaa72dc8b5e2b9858cbee458e2
Kirill <kir.karasyov2017@yandex.ru>
2024-08-20 14:50:07 +0300

    Modofied bintree

--- bintree.go
+++ bintree.go
@@ -50,67 +50,13 @@ func (bt *BinTree[T]) search(node *Node[T], val T) (any, error) {
 	}
 	if bt.comp(val, node.Value) == 0 {
 		return val, nil
-	} else if bt.comp(val, node.Value) < 0 {
-		return bt.search(node.Left, val)
-	} else if bt.comp(val, node.Value) > 0 {
-		return bt.search(node.Right, val)
 	}
+	if bt.comp(val, node.Value) < 0 {
+		return bt.search(node.Left, val)
+	}
+	return bt.search(node.Right, val)
 }
 
 func (bt *BinTree[T]) Search(val T) (any, error) {
 	return bt.search(bt.head, val)
-	// cur_node := bt.head
-	// for {
-	// 	if bt.comp(val, cur_node.Value) == 0 {
-	// 		return val, nil
-	// 	} else if bt.comp(val, cur_node.Value) < 0 {
-	// 		if cur_node.Left == nil {
-	// 			return nil, ErrItemNotFound
-	// 		}
-	// 		cur_node = cur_node.Left
-	// 	} else if bt.comp(val, cur_node.Value) > 0 {
-	// 		if cur_node.Right == nil {
-	// 			return nil, ErrItemNotFound
-	// 		}
-	// 		cur_node = cur_node.Right
-	// 	}
-	// }
-}
-
-// func ComparatorInt(a, b int) int {
-// 	if a > b {
-// 		return 1
-// 	} else if a < b {
-// 		return -1
-// 	}
-// 	return 0
-// }
-
-func (bt *BinTree[T]) Push(t T) {
-	n := newNode(t)
-	bt.items = append(bt.items, *n)
-}
-
-func (bt *BinTree[T]) Pop() (T, error) {
-	l := len(bt.items)
-	var t T
-	if l == 0 {
-		return t, ErrEmptyBinTree
-	}
-	t = bt.items[l-1].Value
-	bt.items = bt.items[:l-1]
-	return t, nil
-}
-
-func (bt *BinTree[T]) Peek() (T, error) {
-	l := len(bt.items)
-	var t T
-	if l == 0 {
-		return t, ErrEmptyBinTree
-	}
-	return bt.items[l-1].Value, nil
-}
-
-func (bt *BinTree[T]) Length() int {
-	return len(bt.items)
 }
