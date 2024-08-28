3b4fee7cd2a5b86d9c361f6f742af76cf8680188
Denis Strizhkin <strdenis02@gmail.com>
2024-08-29 00:47:33 +0300

    finished bintree

--- bintree/tree.go
+++ bintree/tree.go
@@ -1,13 +1,9 @@
 package bintree
 
-import (
-	"fmt"
-)
-
 type BinTree[Key, Value any] struct {
-	head    *Node[Key, Value]
-	comp    Comparator[Key]
-	deleted Value
+	head  *Node[Key, Value]
+	cmp   Comparator[Key]
+	count int
 }
 
 type Node[Key, Value any] struct {
@@ -31,111 +27,104 @@ func NewNode[Key, Value any](key Key, val Value) *Node[Key, Value] {
 // 	return n.Right
 // }
 
-func (n *Node[Key, Value]) IsLeafNode() bool {
+func (n *Node[Key, Value]) IsLeaf() bool {
 	return n.Left == nil && n.Right == nil
 }
 
-func (n *Node[Key, Value]) IsFullNode() bool {
+func (n *Node[Key, Value]) IsFull() bool {
 	return n.Left != nil && n.Right != nil
+}
+
+func (n *Node[Key, Value]) minNode() *Node[Key, Value] {
+	if n.Left == nil {
+		return n
+	}
+	return n.minNode()
 }
 
 type Comparator[Key any] func(a, b Key) int
 
 func NewBinTree[Key, Value any](comp Comparator[Key]) *BinTree[Key, Value] {
-	return &BinTree[Key, Value]{comp: comp}
+	return &BinTree[Key, Value]{cmp: comp}
 }
 
-func (bt *BinTree[Key, Value]) InsertNode(key Key, val Value) {
-	bt.head = bt.insertNode(bt.head, key, val)
+func (bt *BinTree[Key, Value]) Size() int {
+	return bt.count
 }
 
-func (bt *BinTree[Key, Value]) insertNode(
+func (bt *BinTree[Key, Value]) Put(key Key, val Value) {
+	bt.head = bt.put(bt.head, key, val)
+}
+
+func (bt *BinTree[Key, Value]) put(
 	node *Node[Key, Value], key Key, val Value,
 ) *Node[Key, Value] {
 	if node == nil {
-		fmt.Println("Inserted value!")
+		bt.count++
 		return NewNode(key, val)
 	}
-	cmp := bt.comp(node.Key, key)
+	cmp := bt.cmp(node.Key, key)
 	switch {
 	case cmp < 0:
-		return bt.insertNode(node.Left, key, val)
+		return bt.put(node.Left, key, val)
 	case cmp > 0:
-		return bt.insertNode(node.Right, key, val)
+		return bt.put(node.Right, key, val)
 	default:
 		node.Value = val
 		return node
 	}
 }
 
-func (bt *BinTree[Key, Value]) Search(key Key) (Value, bool) {
-	return bt.search(bt.head, key)
+func (bt *BinTree[Key, Value]) Get(key Key) (Value, bool) {
+	return bt.get(bt.head, key)
 }
 
-func (bt *BinTree[Key, Value]) search(node *Node[Key, Value], key Key) (Value, bool) {
+func (bt *BinTree[Key, Value]) get(node *Node[Key, Value], key Key) (Value, bool) {
 	if node == nil {
 		var zero Value
 		return zero, false
 	}
-	cmp := bt.comp(node.Key, key)
+	cmp := bt.cmp(node.Key, key)
 	switch {
 	case cmp < 0:
-		return bt.search(node.Left, key)
+		return bt.get(node.Left, key)
 	case cmp > 0:
-		return bt.search(node.Right, key)
+		return bt.get(node.Right, key)
 	default:
 		return node.Value, true
 	}
 }
 
-func (bt *BinTree[Key, Value]) DeleteNode(key Key) (Value, bool) {
-	var zero Value
-	var ok bool
-	bt.deleted = zero
-	bt.head, ok = bt.delete(bt.head, key)
-	return bt.deleted, ok
+func (bt *BinTree[Key, Value]) Delete(key Key) {
+	bt.head = bt.delete(bt.head, key)
 }
 
-func (bt *BinTree[Key, Value]) delete(node *Node[Key, Value], key Key) (*Node[Key, Value], bool) {
+func (bt *BinTree[Key, Value]) delete(node *Node[Key, Value], key Key) *Node[Key, Value] {
 	if node == nil {
-		return nil, false
+		return nil
 	}
-	cmp := bt.comp(node.Key, key)
+	cmp := bt.cmp(node.Key, key)
 	switch {
 	case cmp < 0:
 		return bt.delete(node.Left, key)
 	case cmp > 0:
 		return bt.delete(node.Right, key)
 	default:
-		bt.deleteAt(node)
-		return node, true
+		bt.count--
+		return bt.deleteAt(node)
 	}
 }
 
-func (bt *BinTree[Key, Value]) deleteAt(node *Node[Key, Value]) {
-	bt.deleted = node.Value
+func (bt *BinTree[Key, Value]) deleteAt(node *Node[Key, Value]) *Node[Key, Value] {
 	switch {
-		case node.Left == nil:
-			return node.Right, true
-		case node.Right == nil:
-			return node.Left, true
-		default:
-			nodeMin := 
-	}
 	case node.Left == nil:
-		return node.Right, true
+		return node.Right
 	case node.Right == nil:
-		bt.deleted = node.Value
-		return node.Left, true
-	// new_n := bt.minNodeSearch(node.Right)
-	// bt.delete(node.Right, new_n.Value)
-	// return new_n, nil
-	
-}
-
-func (bt *BinTree[Key, Value]) minNodeSearch(node *Node[Key, Value]) *Node[Key, Value] {
-	if node.Left == nil {
+		return node.Left
+	default:
+		nodeMin := node.Right.minNode()
+		node.Value = nodeMin.Value
+		bt.deleteAt(nodeMin)
 		return node
 	}
-	return bt.minNodeSearch(node.Left)
 }
