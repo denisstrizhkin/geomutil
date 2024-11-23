83f66ae38237f035f233a43723ba82601c28b90f
Denis Strizhkin <strdenis02@gmail.com>
2024-11-23 22:33:59 +0300

    initialize triangulation

--- triangulation/triangulation_2d.go
+++ triangulation/triangulation_2d.go
@@ -2,8 +2,50 @@ package triangulation
 
 import (
 	util "github.com/denisstrizhkin/geomutil/util"
+	"math"
 )
 
 type Triangle2D struct {
-	A Point2D
+	A util.Point2D
+	B util.Point2D
+	C util.Point2D
+}
+
+func degreesToRadians(deg float32) float32 {
+	return deg * math.Pi / 180.0
+}
+
+func getSuperTriangle(points []util.Point2D) Triangle2D {
+	center := util.Point2DAvg(points)
+	radius := float32(math.Inf(-1))
+	for _, p := range points {
+		dist := center.Distance(p)
+		if dist > radius {
+			radius = dist
+		}
+	}
+	radius += 1.0
+	rad := float64(degreesToRadians(30.0))
+	half_median := radius / float32(math.Sin(rad))
+	half_side := radius / float32(math.Cos(rad))
+	return Triangle2D{
+		util.NewPoint2D(center.X-half_side, center.Y-radius),
+		util.NewPoint2D(center.X+half_side, center.Y-radius),
+		util.NewPoint2D(center.X, center.Y+half_median),
+	}
+}
+
+type Triangulation2D struct {
+	triangles   []Triangle2D
+	points      []util.Point2D
+	edge_points []util.Point2D
+}
+
+func NewTriangulation2D(points []util.Point2D) Triangulation2D {
+	superTriangle := getSuperTriangle(points)
+	return Triangulation2D{
+		triangles:   []Triangle2D{superTriangle},
+		points:      points,
+		edge_points: []util.Point2D{superTriangle.A, superTriangle.B, superTriangle.C},
+	}
 }
--- util/point_2d.go
+++ util/point_2d.go
@@ -1,7 +1,19 @@
 package util
 
+import (
+	"math"
+)
+
 type Point2D struct {
 	X, Y float32
+}
+
+func NewPoint2D(X float32, Y float32) Point2D {
+	return Point2D{X: X, Y: Y}
+}
+
+func (p Point2D) Length() float32 {
+	return float32(math.Sqrt(float64(p.X*p.X + p.Y*p.Y)))
 }
 
 func (p Point2D) Scale(a float32) Point2D {
@@ -14,6 +26,15 @@ func (p Point2D) Add(q Point2D) Point2D {
 
 func (p Point2D) Subtract(q Point2D) Point2D {
 	return Point2D{p.X - q.X, p.Y - q.Y}
+}
+
+func (p Point2D) Multiply(q Point2D) Point2D {
+	return Point2D{p.X * q.Y, p.X * q.Y}
+}
+
+func (p Point2D) Distance(q Point2D) float32 {
+	d := p.Subtract(q)
+	return d.Length()
 }
 
 func Point2DAvg(points []Point2D) Point2D {
